513251d91ab54ffa16b81cd4bb7538947152f26a
DanzinNN <danielguimaraessouza8@gmail.com>
2024-03-26 08:09:58 -0400

    ok

--- lib/Pages/login.dart
+++ lib/Pages/login.dart
@@ -11,28 +11,87 @@ class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center( 
-        child:Column(
+      backgroundColor: Colors.white,
+      body: Stack(
         children: [
-          Container(
-            color: Colors.blueAccent,
-            width: 364,
-            height: 60,
-          ),
-          Container(
-            width: 364,
-            height: 755,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(20)
+          Positioned(
+            top: 60,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20))
+              ),
+              child: Column(
+                children: [
+                  Image.asset("images/logo.png"),
+                  Text("Bem-Vindo",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold
+                    
+                  ),),
+                  Text("Faça login para continuar",
+                  style: TextStyle(
+                    color: Color(0xC1C1C1C2),
+                    fontSize: 10,
+                  ),),
+                  Text("E-mail",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 13
+                  ),),
+                  SizedBox(
+                    width: 318,
+                    height: 32,
+                    child: TextField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text("Senha",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 13,
+                  ),),
+                  SizedBox(
+                    width: 318,
+                    height: 32  ,
+                    child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                    ),
+                  )
+                  ) 
+                ],
               )
-            ),
-          )
+            ) 
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              color: Colors.blueAccent,
+              height: 60,
+            ))
+
         ],
       ),
-      )
     );
   }
 }
